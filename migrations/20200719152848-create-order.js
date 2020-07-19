01abddd40e3cc9c09b89b25ec517fac16ad95aa3
DarianRushworth <darianerrolrushworth@gmail.com>
2020-07-19 17:58:16 +0200

    Models and migrations set-up

---
+++ migrations/20200719152848-create-order.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Order', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.STRING
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      total: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Order');
+  }
+};
